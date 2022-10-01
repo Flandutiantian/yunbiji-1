0d54805afef2e2720ed644f156933048b84074f7
Sonia <1737438545@qq.com>
2022-10-01 21:42:59 +0800

    test

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/yunbiji-website/'
+    : '/',
+
   transpileDependencies: true,
 
   runtimeCompiler: true,
